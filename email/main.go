527fb053b468fe9c86dcd1a7ce107132ec5e35a7
Pavlyysh <pgfu619@mail.ru>
2025-10-30 22:19:56 +0300

    created an email func which uses smtp.mail.ru with app pswd. Sends simplest message to the email. Also used .env file

--- email/main.go
+++ email/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/smtp"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -17,17 +18,32 @@ func main() {
 }
 
 func email() {
+	// sender data
 	from := os.Getenv("FromEmailAddr")
 	password := os.Getenv("SMTPpwd")
 
+	// receiver address
 	toEmail := os.Getenv("ToEmailAddr")
 	to := []string{toEmail}
 
-	host := "smtp.gmail.com"
+	// address setuo
+	host := "smtp.mail.ru"
 	port := "587"
 	address := host + ":" + port
 
-	subject := "Subject: Our golang email\n"
-	body := "our first email"
+	// message
+	subject := "Subject: Our Golang email\n"
+	body := "our first email!"
 	message := []byte(subject + body)
+
+	// authentication data
+	auth := smtp.PlainAuth("", from, password, host)
+
+	// send email
+	err := smtp.SendMail(address, auth, from, to, message)
+	if err != nil {
+		fmt.Println("err:", err)
+		return
+	}
+	fmt.Println("check the email")
 }
